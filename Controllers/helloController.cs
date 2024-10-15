b87732a72e9633e2635e365dbed8703d0f4050d9
Bragathambal <154111982+Bragathambal@users.noreply.github.com>
2024-10-15 15:06:16 +0530

    Add files via upload

---
+++ Controllers/helloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCsampleapp.Controllers
+{
+    public class helloController : Controller
+    {
+        // GET: hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Home()
+        {
+            return "this is second router";
+        }
+      
+    }
+}
